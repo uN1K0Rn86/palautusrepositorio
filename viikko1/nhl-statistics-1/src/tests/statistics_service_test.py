53a6b8f255490c6dfc33a2031822d10e1015d394
Sami Heinimo <sami.heinimo@helsinki.fi>
2024-11-04 20:02:25 +0200

    Added tests for player search.

---
+++ viikko1/nhl-statistics-1/src/tests/statistics_service_test.py
@@ -0,0 +1,28 @@
+import unittest
+from statistics_service import StatisticsService
+from player import Player
+
+class PlayerReaderStub:
+    def get_players(self):
+        return [
+            Player("Semenko", "EDM", 4, 12),
+            Player("Lemieux", "PIT", 45, 54),
+            Player("Kurri",   "EDM", 37, 53),
+            Player("Yzerman", "DET", 42, 56),
+            Player("Gretzky", "EDM", 35, 89)
+        ]
+
+class TestStatisticsService(unittest.TestCase):
+    def setUp(self):
+        # annetaan StatisticsService-luokan oliolle "stub"-luokan olio
+        self.stats = StatisticsService(
+            PlayerReaderStub()
+        )
+
+    def test_search_found(self):
+        jerry = self.stats.search("Kurri")
+        self.assertEqual(jerry.name, Player("Kurri", "EDM", 37, 53).name)
+
+    def test_search_none(self):
+        modano = self.stats.search("Modano")
+        self.assertEqual(modano, None)
