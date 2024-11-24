35a067ff634a04f8b62e2a4d01ceed804b39787c
Sami Heinimo <sami.heinimo@helsinki.fi>
2024-11-24 15:54:55 +0200

    Added test for aloita_asiointi

--- viikko4/verkkokauppa/src/tests/kauppa_test.py
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -162,3 +162,34 @@ class TestKauppa(unittest.TestCase):
         kauppa.tilimaksu("sami", "80085")
 
         self.pankki_mock.tilisiirto.assert_called_with("sami", 99, "80085", "33333-44455", 9)
+
+    def test_aloita_asiointi_nollaa_edellisen_ostoksen(self):
+        self.viitegeneraattori_mock.uusi.return_value = 99
+
+        # tehdään toteutus saldo-metodille
+        def varasto_saldo(tuote_id):
+            if tuote_id == 1:
+                return 0
+            if tuote_id == 3:
+                return 15
+
+        # tehdään toteutus hae_tuote-metodille
+        def varasto_hae_tuote(tuote_id):
+            if tuote_id == 1:
+                return Tuote(1, "maito", 5)
+            if tuote_id == 3:
+                return Tuote(3, "mosaic_ipa", 9)
+
+        # otetaan toteutukset käyttöön
+        self.varasto_mock.saldo.side_effect = varasto_saldo
+        self.varasto_mock.hae_tuote.side_effect = varasto_hae_tuote
+
+        # alustetaan kauppa
+        kauppa = Kauppa(self.varasto_mock, self.pankki_mock, self.viitegeneraattori_mock)
+
+        kauppa.aloita_asiointi()
+        kauppa.lisaa_koriin(1)
+        kauppa.lisaa_koriin(3)
+
+        kauppa.aloita_asiointi()
+        self.assertEqual(kauppa._ostoskori.hinta(), 0)
