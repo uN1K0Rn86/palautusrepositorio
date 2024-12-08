32818345f008f55f9b5523492aabe29d51414379
Sami Heinimo <sami.heinimo@helsinki.fi>
2024-12-08 14:40:27 +0200

    Week 6 exercise 4

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,21 +1,15 @@
 from statistics import Statistics
 from player_reader import PlayerReader
 from matchers import And, Or, All, Not, HasFewerThan, HasAtLeast, PlaysIn
+from querybuilder import QueryBuilder
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-    HasAtLeast(70, "points"),
-    Or(
-        PlaysIn("NYR"),
-        PlaysIn("FLA"),
-        PlaysIn("BOS")
-    )
-    )
-
+    query = QueryBuilder()
+    matcher = query.plays_in("NYR").has_at_least(10, "goals").has_fewer_than(20, "goals").build()
 
     for player in stats.matches(matcher):
         print(player)
---
+++ viikko6/query-language/src/querybuilder.py
@@ -0,0 +1,19 @@
+from statistics import Statistics
+from player_reader import PlayerReader
+from matchers import And, Or, All, Not, HasFewerThan, HasAtLeast, PlaysIn
+
+class QueryBuilder:
+    def __init__(self, matcher=All()):
+        self._matcher = matcher
+
+    def build(self):
+        return self._matcher
+
+    def plays_in(self, team):
+        return QueryBuilder(And(self._matcher, PlaysIn(team)))
+    
+    def has_at_least(self, value, attr):
+        return QueryBuilder(And(self._matcher, HasAtLeast(value, attr)))
+    
+    def has_fewer_than(self, value, attr):
+        return QueryBuilder(And(self._matcher, HasFewerThan(value, attr)))
