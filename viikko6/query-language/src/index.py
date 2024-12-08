fe8b8ced1fd91d56fbce1066da3484ddf3569f76
Sami Heinimo <sami.heinimo@helsinki.fi>
2024-12-08 13:34:40 +0200

    Exercise 2 week 6

---
+++ viikko6/query-language/src/index.py
@@ -0,0 +1,25 @@
+from statistics import Statistics
+from player_reader import PlayerReader
+from matchers import And, All, Not, HasFewerThan, HasAtLeast, PlaysIn
+
+def main():
+    url = "https://studies.cs.helsinki.fi/nhlstats/2023-24/players.txt"
+    reader = PlayerReader(url)
+    stats = Statistics(reader)
+
+    matcher = And(
+    HasFewerThan(2, "goals"),
+    PlaysIn("NYR")
+    )
+
+
+
+    for player in stats.matches(matcher):
+        print(player)
+
+    filtered_with_all = stats.matches(All())
+    print(len(filtered_with_all))
+
+
+if __name__ == "__main__":
+    main()
